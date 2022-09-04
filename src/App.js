332e4246af6d74f6090e4456852b6be392edc9fd
smitkalwal <smitkalwal@gmail.com>
2022-09-04 13:20:12 +0530

    rmoeved socket

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
-import socketIO from 'socket.io-client';
-const socket = socketIO.connect('http://localhost:4000');
+// import socketIO from 'socket.io-client';
+// const socket = socketIO.connect('http://localhost:4000');
 function App() {
   return (
     <div>
